478239084152b666b3df101419e0977bb379c6f8
Abhishek Anand <ab.work2103@gmail.com>
2026-07-28 22:57:34 +0530

    started a react series of mini projects and made a Card component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Card } from './components/Card'
+import './App.css'
+
+function App() {
+  return(
+    <Card
+      name="Abhishek Anand" 
+      role="FullStack Developer"
+      email="xyz@gmail.com "
+    />
+
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+
+export function Card({ name, role, email }) {
+  return(
+    <>
+      <h2>{name}</h2>
+      <p>{role}</p>
+      <p>{email}</p>
+    </>
+  )
+}
