4877eb4d4ffe359d6e4a38034e7f81e3458e8e0b
Abhishek Anand <ab.work2103@gmail.com>
2026-08-02 10:56:30 +0530

    update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 export function Card({ name, role, email }) {
   return(
     <>
-      <h2>{name}</h2>
-      <p>{role}</p>
-      <p>{email}</p>
+      <h2>👤 {name}</h2>
+      <p>💼 {role}</p>
+      <p>📧 {email}</p>
     </>
   )
 }
